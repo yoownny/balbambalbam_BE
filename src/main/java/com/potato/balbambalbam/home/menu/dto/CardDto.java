c29bac68140bca70569a60c6f3338848c2171d25
gabean13 <choegabin171@gmail.com>
2024-10-26 01:34:45 +0900

    [Fix] swagger name -> description으로 변경

--- src/main/java/com/potato/balbambalbam/home/menu/dto/CardDto.java
+++ src/main/java/com/potato/balbambalbam/home/menu/dto/CardDto.java
@@ -11,21 +11,21 @@ import lombok.*;
 @Schema(description = "카드리스트 각 카드 정보 Response")
 public class CardDto {
 
-    @Schema(name = "card Id", example = "1")
+    @Schema(description = "card Id", example = "1")
     private Long id;
 
-    @Schema(name = "card 내용", example = "아")
+    @Schema(description = "card 내용", example = "아")
     private String text;
 
-    @Schema(name = "card 로마자 발음", example = "a")
+    @Schema(description = "card 로마자 발음", example = "a")
     private String cardPronunciation;
 
-    @Schema(name = "card 북마크 유뮤", example = "true")
+    @Schema(description = "card 북마크 유뮤", example = "true")
     private boolean isBookmarked;
 
-    @Schema(name = "card 취약음 유뮤", example = "true")
+    @Schema(description = "card 취약음 유뮤", example = "true")
     private boolean isWeakCard;
 
-    @Schema(name = "card 최고 점수", example = "80")
+    @Schema(description = "card 최고 점수", example = "80")
     private int cardScore;
 }
