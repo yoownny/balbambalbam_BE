c6b478f8daa1ac7ea5284ee840271495130de0cb
yoownny <kelosy00@naver.com>
2024-10-26 02:40:22 +0900

    [Fix] : Merge 오류 수정

--- src/main/java/com/potato/balbambalbam/user/home/controller/HomeController.java
+++ src/main/java/com/potato/balbambalbam/user/home/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.potato.balbambalbam.user.home.controller;
 
 import com.potato.balbambalbam.exception.dto.ExceptionDto;
 import com.potato.balbambalbam.user.home.dto.HomeInfoDto;
+import com.potato.balbambalbam.user.home.service.HomeInfoService;
 import com.potato.balbambalbam.user.join.service.JoinService;
 import com.potato.balbambalbam.user.token.jwt.JWTUtil;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,22 +11,20 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @Tag(name = "Home API", description = "홈 화면에 필요한 사용자 레벨, 레벨 경험치, 사용자 경험치, 주간 출석 상황, 오늘의 추천 단어를 반환한다. ")
 public class HomeController {
 
     private final JoinService joinService;
     private final JWTUtil jwtUtil;
-
-    public HomeController(JoinService joinService, JWTUtil jwtUtil) {
-        this.joinService = joinService;
-        this.jwtUtil = jwtUtil;
-    }
+    private final HomeInfoService homeInfoService;
 
     private Long extractUserIdFromToken(String access) { // access 토큰으로부터 userId 추출하는 함수
         String socialId = jwtUtil.getSocialId(access);
@@ -41,8 +40,7 @@ public class HomeController {
     @GetMapping("/home")
     public ResponseEntity<HomeInfoDto> getHomeInfo(@RequestHeader("access") String access) {
         Long userId = extractUserIdFromToken(access);
-
-        HomeInfoDto response = new HomeInfoDto();
+        HomeInfoDto response = homeInfoService.getHomeInfo(userId);
         return ResponseEntity.ok(response);
     }
 }
