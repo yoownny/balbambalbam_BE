1cdcc9b4ff96c7d341c67da9e6397ae4f3be0d99
yoownny <kelosy00@naver.com>
2024-12-11 23:00:44 +0900

    feat: 취약음소 에러처리 기능 추가

--- src/main/java/com/potato/balbambalbam/myReport/test/service/WeakSoundTestAiService.java
+++ src/main/java/com/potato/balbambalbam/myReport/test/service/WeakSoundTestAiService.java
@@ -2,10 +2,13 @@ package com.potato.balbambalbam.myReport.test.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.potato.balbambalbam.exception.AiGenerationFailException;
+import com.potato.balbambalbam.exception.InvalidParameterException;
 import com.potato.balbambalbam.myReport.test.dto.TestResponseDto;
 import com.potato.balbambalbam.myReport.weaksound.service.PhonemeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -38,6 +41,15 @@ public class WeakSoundTestAiService {
                 .contentType(MediaType.APPLICATION_JSON)
                 .bodyValue(testRequestJson)
                 .retrieve()
+                //에러 처리 : 요청이 잘못갔을 경우
+                .onStatus(HttpStatus.BAD_REQUEST::equals,
+                        response -> response.bodyToMono(String.class).map(InvalidParameterException::new))
+                //에러 처리 : 요청이 잘못갔을 경우
+                .onStatus(HttpStatus.UNPROCESSABLE_ENTITY::equals,
+                        response -> response.bodyToMono(String.class).map(InvalidParameterException::new))
+                //에러 처리 : 응답 생성 실패
+                .onStatus(HttpStatus.INTERNAL_SERVER_ERROR::equals,
+                        response -> response.bodyToMono(String.class).map(AiGenerationFailException::new))
                 .bodyToMono(String.class)
                 .block();
         TestResponseDto weakSoundTestDto = objectMapper.readValue(testResponseJson, TestResponseDto.class);
